740af32aea27ea7c81a9839b4b379028d9041535
Rifad <rifad@kurio.co.id>
2015-08-07 14:06:29 +0700

    Initial commit

---
+++ src/HttpServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+namespace Kurio\HttpClient;
+
+use Illuminate\Support\ServiceProvider;
+use Ivory\HttpAdapter\Event\Subscriber\RedirectSubscriber;
+use Ivory\HttpAdapter\HttpAdapterFactory;
+use Ivory\HttpAdapter\EventDispatcherHttpAdapter;
+use Ivory\HttpAdapter\PsrHttpAdapterInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+class HttpServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $http_adapter = $this->getHttpAdapter();
+        $event_dispatcher = $this->getEventDispatcher();
+
+        $http = new EventDispatcherHttpAdapter($http_adapter, $event_dispatcher);
+        $this->app->bind('http', $http);
+    }
+
+    /**
+     * @return PsrHttpAdapterInterface
+     */
+    protected function getHttpAdapter()
+    {
+        $preferred_library = 'guzzle_http';
+        return HttpAdapterFactory::guess($preferred_library);
+    }
+
+    /**
+     * @return EventDispatcher
+     */
+    protected function getEventDispatcher()
+    {
+        $dispatcher = new EventDispatcher;
+
+        $redirect_subsciber = new RedirectSubscriber;
+        $dispatcher->addSubscriber($redirect_subsciber);
+
+        return $dispatcher;
+    }
+}
